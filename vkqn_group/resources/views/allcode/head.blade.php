a946a5482f493b4f162cf6ccddc1150d244bc25b
KIEND3 <phamcongkienqn@gmail.com>
2023-02-23 23:40:47 +0700

    Update head.blade.php

--- vkqn_group/resources/views/allcode/head.blade.php
+++ vkqn_group/resources/views/allcode/head.blade.php
@@ -6,4 +6,4 @@
     <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="icon" type="image/png" href="https://png.pngtree.com/png-clipart/20190617/original/pngtree-technology-open-icon-ui-png-image_3840639.jpg">
-    <title>Dream Team</title>
+    <title>VKQN Group</title>
