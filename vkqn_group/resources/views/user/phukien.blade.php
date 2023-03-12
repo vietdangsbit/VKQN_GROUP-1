473e7e7a2585f1d16b0c0fb1d51818a5ff9ae339
viet19dthd3 <97188868+viet19dthd3@users.noreply.github.com>
2023-03-12 15:35:17 +0700

    Update phukien.blade.php

--- vkqn_group/resources/views/user/phukien.blade.php
+++ vkqn_group/resources/views/user/phukien.blade.php
@@ -43,7 +43,7 @@
                     <div class="product_phukien_item">
                         <a href="">
                             <div class="product_phukien_icon active"><i class="fa fa-medal"></i></div>
-                            <div class="product_phukien_text">Khuyến mãi hot</div>
+                            <div class="product_phukien_text">Khuyến mãi đặc biệt</div>
                         </a>
                     </div>
 
